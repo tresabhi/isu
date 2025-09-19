c38cf3e9ab6724b8ccea1fbce085336a0637d5a2
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-09-19 10:39:13 -0500

    4

--- aere-362/labs/4/problem_4.m
+++ aere-362/labs/4/problem_4.m
@@ -5,12 +5,14 @@ x = [0; 0; 0];
 
 for i = 1:10
     A = [6, 2 * x(1), x(1); 2 * x(1), 5, 2; 1, x(2), 5];
-    r = A * x - b;
+    L = tril(A);
+    T = triu(A, 1);
 
+    r = A * x - b;
     disp("i = " + num2str(i));
     disp(r);
 
-    x = A \ b;
+    x = L \ (b - T * x);
 end
 
 disp(x);
