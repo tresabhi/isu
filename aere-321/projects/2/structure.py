989a51032903324b750f748615846298a8d2a2c3
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-11-01 15:49:21 -0500

    d

--- aere-321/projects/2/structure.py
+++ aere-321/projects/2/structure.py
@@ -70,14 +70,7 @@ class Member:
                     -((A * L**2 / I) * s**2 + 12 * c**2),
                     6 * L * c,
                 ],
-                [
-                    -6 * L * s,
-                    6 * L * c,
-                    4 * L**2,
-                    6 * L * s,
-                    -6 * L * c,
-                    2 * L**2,
-                ],
+                [-6 * L * s, 6 * L * c, 4 * L**2, 6 * L * s, -6 * L * c, 2 * L**2],
                 [
                     -((A * L**2 / I) * c**2 + 12 * s**2),
                     -((A * L**2 / I) - 12) * c * s,
@@ -94,14 +87,7 @@ class Member:
                     (A * L**2 / I) * s**2 + 12 * c**2,
                     -6 * L * c,
                 ],
-                [
-                    -6 * L * s,
-                    6 * L * c,
-                    2 * L**2,
-                    6 * L * s,
-                    -6 * L * c,
-                    4 * L**2,
-                ],
+                [-6 * L * s, 6 * L * c, 2 * L**2, 6 * L * s, -6 * L * c, 4 * L**2],
             ]
         )
 
@@ -119,38 +105,52 @@ class Member:
 
 class Structure:
     def __init__(
-        self,
-        material: Material,
-        joints: list[Joint],
-        members: list[tuple[int, int]],
+        self, material: Material, joints: list[Joint], members: list[tuple[int, int]]
     ):
         member_id = 0
         joints_count = len(joints)
         max_joint_id = joints_count - 1
         max_member_id = max_joint_id - 1
 
-        S = np.zeros((3 * joints_count, 3 * joints_count))
+        S_padded = np.zeros((3 * joints_count, 3 * joints_count))
 
         for joint_id_0, joint_id_1 in members:
             joint_0 = joints[joint_id_0]
             joint_1 = joints[joint_id_1]
             member = Member(member_id, material, joint_0, joint_1, max_member_id)
 
-            S += member.K_padded
-
+            S_padded += member.K_padded
             member_id += 1
 
         free_indices: list[int] = []
         fixed_indices: list[int] = []
+        P_padded: list[list[float]] = []
 
-        print(S)
+        for joint_id in range(joints_count):
+            joint = joints[joint_id]
+            external_indices = [joint_id * 3, joint_id * 3 + 1, joint_id * 3 + 2]
+
+            P_padded += [[joint.force_x], [joint.force_y], [joint.moment]]
+
+            if joint.type == JointType.FREE:
+                free_indices += external_indices
+            elif joint.type == JointType.FIXED:
+                fixed_indices += external_indices
+
+        S = S_padded[np.ix_(free_indices, free_indices)]
+        P_padded = np.matrix(P_padded)
+        P = P_padded[np.ix_(free_indices)]
+        d = np.linalg.solve(S, P)
+
+        d_padded = np.zeros((3 * joints_count, 1))
+        d_padded[np.ix_(free_indices)] = d
 
 
 lecture_example_1 = Structure(
     Material(29000, 310, 11.8),
     [
         Joint(JointType.FIXED, 0, 0),
-        Joint(JointType.FREE, 10 * 12, 20 * 12, 50, 0, -125),
+        Joint(JointType.FREE, 10 * 12, 20 * 12, 50, 0, -125 * 12),
         Joint(JointType.FIXED, (10 + 20) * 12, 20 * 12),
     ],
     [
