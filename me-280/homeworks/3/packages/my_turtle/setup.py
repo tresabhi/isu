1caaf400b611cc1a9440c9b838c7278a63a0f564
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-11-03 22:40:48 +0000

    launch script

--- me-280/homeworks/3/packages/my_turtle/setup.py
+++ me-280/homeworks/3/packages/my_turtle/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = "my_turtle"
 
@@ -9,6 +11,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name), glob("launch/*.launch")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
