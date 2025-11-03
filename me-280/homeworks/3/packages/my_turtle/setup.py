d1e77fa146be9efa21b5b4eadd62a5eab341bdc5
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-11-03 22:20:27 +0000

    create my_turtle

---
+++ me-280/homeworks/3/packages/my_turtle/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_turtle'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='exouser',
+    maintainer_email='43380238+tresabhi@users.noreply.github.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
