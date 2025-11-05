c1e29a282dc08b8c1b193cb8981378bf51197268
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-11-05 16:01:17 -0600

    p3

---
+++ aere-362/homeworks/5/problem_3.m
@@ -0,0 +1,25 @@
+x0 = [1; 2; 3];
+
+% h = x(1) + 2 * x(2) = 6
+% g1 = x(1) - 2 * x(2) + 2 * x(3) <= 4
+% g2 = x(2) + 3 * x(3) <= 6
+% g3 = x(1) ^ 3 + x(2) ^ 2 - 2 * x(1) * x(3) <= 0
+% g4 = 3 * x(1) - 2 * x(2) <= 2
+
+A = [1, -2, 2; 0, 1, 3; 3, -2, 0];
+b = [4; 6; 2];
+
+Aeq = [1, 2, 0];
+beq = 6;
+
+options = optimoptions("fmincon", "Display", "iter", "Algorithm", "sqp");
+[xOpt, fOpt, exitFlag, output, lambda] = fmincon(@objFunc, x0, A, b, Aeq, beq, [], [], @nlCon, options);
+
+function [c, ceq] = nlCon(x)
+    c(1) = x(1) ^ 3 + x(2) ^ 2 - 2 * x(1) * x(3);
+    ceq = [];
+end
+
+function f = objFunc(x)
+    f = x(1) ^ 2 + 2 * x(2) ^ 2 + 2 * x(3) ^ 2 - 2 * x(1) * x(3) - x(1) * x(2);
+end
