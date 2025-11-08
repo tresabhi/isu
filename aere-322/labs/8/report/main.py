72d5a665ccad6afe401fd2d282aa488d8db2adac
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-11-07 22:29:15 -0600

    some normalization

--- aere-322/labs/8/report/main.py
+++ aere-322/labs/8/report/main.py
@@ -4,8 +4,9 @@ import pint
 ur = pint.UnitRegistry()
 
 i = ur.inch
-flipped_ruler_length = 6 * i
-blue_ruler_padding = (1 / 16) * i
+flipped_ruler = 6 * i
+ruler_padding = (1 / 16) * i
+crossbar_length = 44 * ur.cm
 
 specimens = [
     ("c_channel", 2.43 * i, 1.456 * i, 0.08 * i, None, None),
@@ -15,81 +16,90 @@ specimens = [
     ("circular_open", None, None, 0.071 * i, 1.66 * i, 103.7 * ur.deg),
 ]
 
-experiment_setups = [
-    (0.1 * ur.kg, 7 * ur.mm, 6.8 * ur.mm),
-    (0.2 * ur.kg, (1 + 9 / 16) * i, (1 + 3 / 8) * i),
-    (0.1 * ur.kg, (1 + 1 / 4) * i, 1 * i),
-    (0.1 * ur.kg, (1 + 7 / 16) * 2 * i),
-    (0.1 * ur.kg, (1 + 2 / 5) * i, (1 + 2 / 5) * i),
+setups = [
+    (0.1 * ur.kg, 7 * ur.mm, 6.8 * ur.mm, "open_right"),
+    (0.2 * ur.kg, (1 + 9 / 16) * i, (1 + 3 / 8) * i, "open_left"),
+    (0.1 * ur.kg, (1 + 1 / 4) * i, 1 * i, "open_left"),
+    (0.1 * ur.kg, (1 + 7 / 16) * i, 2 * i, "open_right"),
+    (0.1 * ur.kg, (1 + 2 / 5) * i, (1 + 2 / 5) * i, "open_left"),
 ]
 
-torsion_experiments = [
+experiments = [
     [
         (
-            "R",
+            "to_right",
             2 * i,
             (3 + 9 / 16) * i,
-            flipped_ruler_length - (6 + 1 / 8) * i + blue_ruler_padding,
+            flipped_ruler - (6 + 1 / 8) * i + ruler_padding,
         ),
-        ("L", (4 + 1 / 4) * i, (1 + 6 / 8) * i, (1 + 1 / 8) * i),
+        ("to_left", (4 + 1 / 4) * i, (1 + 6 / 8) * i, (1 + 1 / 8) * i),
         (
-            "L",
-            flipped_ruler_length - (7 + 7 / 16) * i + blue_ruler_padding,
+            "to_left",
+            flipped_ruler - (7 + 7 / 16) * i + ruler_padding,
             (2 + 9 / 16) * i,
             0,
         ),
     ],
     [
-        ("L", 3 * i, (2 + 6 / 8) * i, (11 / 16) * i),
-        ("R", 3 * i, (15 / 16) * i, (2 + 1 / 4) * i),
-        ("R", 4.5 * i, (7 / 16) * i, (2 + 3 / 4) * i),
+        ("to_left", 3 * i, (2 + 6 / 8) * i, (11 / 16) * i),
+        ("to_right", 3 * i, (15 / 16) * i, (2 + 1 / 4) * i),
+        ("to_right", 4.5 * i, (7 / 16) * i, (2 + 3 / 4) * i),
     ],
     [
-        ("L", 1.5 * i, (2 + 1 / 8) * i, (3 / 8) * i),
-        ("R", 4 * i, (1 / 4) * i, (2 + 5 / 8) * i),
-        ("L", 3 * i, (2 + 3 / 8) * i, (1 / 4) * i),
+        ("to_left", 1.5 * i, (2 + 1 / 8) * i, (3 / 8) * i),
+        ("to_right", 4 * i, (1 / 4) * i, (2 + 5 / 8) * i),
+        ("to_left", 3 * i, (2 + 3 / 8) * i, (1 / 4) * i),
     ],
     [
-        ("R", 4 * i, 1 * i, (3 + 3 / 8) * i),
-        ("L", (3 + 1 / 4) * i, (2 + 9 / 16) * i, (1 + 3 / 8) * i),
-        ("L", 2.5 * i, (2 + 7 / 16) * i, (1 + 9 / 16) * i),
+        ("to_right", 4 * i, 1 * i, (3 + 3 / 8) * i),
+        ("to_left", (3 + 1 / 4) * i, (2 + 9 / 16) * i, (1 + 3 / 8) * i),
+        ("to_left", 2.5 * i, (2 + 7 / 16) * i, (1 + 9 / 16) * i),
     ],
     [
-        ("R", (2 + 3 / 10) * i, (3 / 10) * i, (2 + 3 / 10) * i),
-        ("L", 2 * i, (2 + 7 / 10) * i, (3 / 10) * i),
-        ("R", (3 + 3 / 10) * i, 0, (2 + 7 / 10) * i),
+        ("to_right", (2 + 3 / 10) * i, (3 / 10) * i, (2 + 3 / 10) * i),
+        ("to_left", 2 * i, (2 + 7 / 10) * i, (3 / 10) * i),
+        ("to_right", (3 + 3 / 10) * i, 0, (2 + 7 / 10) * i),
     ],
 ]
 
-for side, x, h_l, h_r in torsion_experiments[3]:
-    torsion_experiments[3] = ("L" if side == "R" else "R", x, h_r, h_l)
+# convert all to open_right to match diagram
 
-index = 0
+i = 0
+for setup in setups:
+    (m, l, r, open_side) = setup
+
+    if open_side == "open_left":
+        setups[i] = (m, l, r, "open_right")
+
+        j = 0
+        for experiment in experiments[i]:
+            (read_direction, x, l, r) = experiment
+            read_direction = "to_left" if read_direction == "to_right" else "to_right"
+            l, r = r, l
+            experiments[i][j] = (read_direction, x, l, r)
+            j += 1
+
+    i += 1
+
+
+def theta(l_inverted, r_inverted):
+    l = flipped_ruler - l_inverted
+    r = flipped_ruler - r_inverted
+
+    return math.asin((r - l) / crossbar_length) * ur.rad
+
+
+i = 0
 for specimen in specimens:
-    id = index + 1
+    id = i + 1
 
     (type, h, b, t, d, two_theta) = specimen
-    (m) = experiment_setups[index]
-    w = m * 9.81 * ur.m / ur.s**2
-    w = w.to(ur.lbf)
+    (m, l_0, r_0, open_side) = setups[i]
 
-    e: float
+    theta_0 = theta(l_0, r_0)
 
-    if type == "c_channel":
-        I = (1 / 2) * b * h**2 * t + (1 / 6) * b * t**3 + (1 / 12) * t * (h - t) ** 3
-        e = (h**2 * b**2 * t) / (4 * I)
-    elif type == "circular_open":
-        theta = two_theta / 2
+    for experiment in experiments[i]:
+        (read_direction, x, l, r) = experiment
+        theta_i = theta(l, r)
 
-        r = d / 2 - t / 2
-        e = (
-            2 * r * (math.cos(theta) * (2 * math.pi - 2 * theta) + 2 * math.sin(theta))
-        ) / (2 * math.pi - 2 * theta + math.sin(2 * theta))
-    else:
-        raise ValueError(f"Unknown type: {type}")
-
-    print(f"e_{id} = {e}")
-    print(f"w_{id} = {w}")
-    print()
-
-    index += 1
+    i += 1
