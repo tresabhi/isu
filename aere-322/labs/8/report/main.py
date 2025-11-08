57334b911002b96a49277cdce59509867f458eee
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-11-07 23:07:53 -0600

    fix i and inches mix up

--- aere-322/labs/8/report/main.py
+++ aere-322/labs/8/report/main.py
@@ -4,52 +4,52 @@ import numpy as np
 
 ur = pint.UnitRegistry()
 
-i = ur.inch
-flipped_ruler = 6 * i
-ruler_padding = (1 / 16) * i
+inch = ur.inch
+flipped_ruler = 6 * inch
+ruler_padding = (1 / 16) * inch
 crossbar_length = 44 * ur.cm
 
 specimens = [
-    ("c_channel", 2.43 * i, 1.456 * i, 0.08 * i, None, None),
-    ("c_channel", 0.84 * i, 0.56 * i, 0.055 * i, None, None),
-    ("circular_open", None, None, 0.071 * i, 1.66 * i, 3.1 * ur.deg),
-    ("circular_open", None, None, 0.071 * i, 1.66 * i, 36.3 * ur.deg),
-    ("circular_open", None, None, 0.071 * i, 1.66 * i, 103.7 * ur.deg),
+    ("c_channel", 2.43 * inch, 1.456 * inch, 0.08 * inch, None, None),
+    ("c_channel", 0.84 * inch, 0.56 * inch, 0.055 * inch, None, None),
+    ("circular_open", None, None, 0.071 * inch, 1.66 * inch, 3.1 * ur.deg),
+    ("circular_open", None, None, 0.071 * inch, 1.66 * inch, 36.3 * ur.deg),
+    ("circular_open", None, None, 0.071 * inch, 1.66 * inch, 103.7 * ur.deg),
 ]
 
 setups = [
     (0.1 * ur.kg, 7 * ur.mm, 6.8 * ur.mm, "open_right"),
-    (0.2 * ur.kg, (1 + 9 / 16) * i, (1 + 3 / 8) * i, "open_left"),
-    (0.1 * ur.kg, (1 + 1 / 4) * i, 1 * i, "open_left"),
-    (0.1 * ur.kg, (1 + 7 / 16) * i, 2 * i, "open_right"),
-    (0.1 * ur.kg, (1 + 2 / 5) * i, (1 + 2 / 5) * i, "open_left"),
+    (0.2 * ur.kg, (1 + 9 / 16) * inch, (1 + 3 / 8) * inch, "open_left"),
+    (0.1 * ur.kg, (1 + 1 / 4) * inch, 1 * inch, "open_left"),
+    (0.1 * ur.kg, (1 + 7 / 16) * inch, 2 * inch, "open_right"),
+    (0.1 * ur.kg, (1 + 2 / 5) * inch, (1 + 2 / 5) * inch, "open_left"),
 ]
 
 experiments = [
     [
-        ("to_right", 2 * i, (3 + 9 / 16) * i, flipped_ruler - (6 + 1 / 8) * i),
-        ("to_left", (4 + 1 / 4) * i, (1 + 6 / 8) * i, (1 + 1 / 8) * i),
-        ("to_left", (7 + 7 / 16) * i + ruler_padding, (2 + 9 / 16) * i, 0),
+        ("to_right", 2 * inch, (3 + 9 / 16) * inch, (6 + 1 / 8) * inch),
+        ("to_left", (4 + 1 / 4) * inch, (1 + 6 / 8) * inch, (1 + 1 / 8) * inch),
+        ("to_left", (7 + 7 / 16) * inch + ruler_padding, (2 + 9 / 16) * inch, 0),
     ],
     [
-        ("to_left", 3 * i, (2 + 6 / 8) * i, (11 / 16) * i),
-        ("to_right", 3 * i, (15 / 16) * i, (2 + 1 / 4) * i),
-        ("to_right", 4.5 * i, (7 / 16) * i, (2 + 3 / 4) * i),
+        ("to_left", 3 * inch, (2 + 6 / 8) * inch, (11 / 16) * inch),
+        ("to_right", 3 * inch, (15 / 16) * inch, (2 + 1 / 4) * inch),
+        ("to_right", 4.5 * inch, (7 / 16) * inch, (2 + 3 / 4) * inch),
     ],
     [
-        ("to_left", 1.5 * i, (2 + 1 / 8) * i, (3 / 8) * i),
-        ("to_right", 4 * i, (1 / 4) * i, (2 + 5 / 8) * i),
-        ("to_left", 3 * i, (2 + 3 / 8) * i, (1 / 4) * i),
+        ("to_left", 1.5 * inch, (2 + 1 / 8) * inch, (3 / 8) * inch),
+        ("to_right", 4 * inch, (1 / 4) * inch, (2 + 5 / 8) * inch),
+        ("to_left", 3 * inch, (2 + 3 / 8) * inch, (1 / 4) * inch),
     ],
     [
-        ("to_right", 4 * i, 1 * i, (3 + 3 / 8) * i),
-        ("to_left", (3 + 1 / 4) * i, (2 + 9 / 16) * i, (1 + 3 / 8) * i),
-        ("to_left", 2.5 * i, (2 + 7 / 16) * i, (1 + 9 / 16) * i),
+        ("to_right", 4 * inch, 1 * inch, (3 + 3 / 8) * inch),
+        ("to_left", (3 + 1 / 4) * inch, (2 + 9 / 16) * inch, (1 + 3 / 8) * inch),
+        ("to_left", 2.5 * inch, (2 + 7 / 16) * inch, (1 + 9 / 16) * inch),
     ],
     [
-        ("to_right", (2 + 3 / 10) * i, (3 / 10) * i, (2 + 3 / 10) * i),
-        ("to_left", 2 * i, (2 + 7 / 10) * i, (3 / 10) * i),
-        ("to_right", (3 + 3 / 10) * i, 0, (2 + 7 / 10) * i),
+        ("to_right", (2 + 3 / 10) * inch, (3 / 10) * inch, (2 + 3 / 10) * inch),
+        ("to_left", 2 * inch, (2 + 7 / 10) * inch, (3 / 10) * inch),
+        ("to_right", (3 + 3 / 10) * inch, 0, (2 + 7 / 10) * inch),
     ],
 ]
 
@@ -109,17 +109,26 @@ for specimen in specimens:
         (read_direction, x_raw, l, r) = experiment
 
         x = x_raw + t / 2 if read_direction == "to_right" else -x_raw - t / 2
-        theta = angle(l, r)
+        theta = angle(l, r) - theta_0
 
         xs.append(x.to(ur.inch).magnitude)
-        thetas.append(theta.to(ur.deg).magnitude)
+        thetas.append(theta.to(ur.rad).magnitude)
 
-        print(f"({xs[-1]}, {thetas[-1]})")
+        print(
+            f"x = {xs[-1]:.4f} in, theta = {theta.to(ur.deg).magnitude:.4f} deg, "
+            f"theta_rel = {theta.to(ur.deg)}"
+        )
 
     a, b = np.polyfit(xs, thetas, 1)
-    e_experimental = -b / a
+    e_experimental = (-b / a) * inch
 
-    print(e, e_experimental)
+    print()
+
+    print("slope (rad/in) =", a)
+    print("intercept (rad) =", b)
+    print("e (experimental) =", e_experimental)
+    print("e (theoretical)  =", e)
+
     print()
 
     i += 1
