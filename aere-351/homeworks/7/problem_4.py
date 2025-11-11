6d4cafd586f24309308de01056551262cce2d8e6
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-11-11 17:20:10 -0600

    fix signs

--- aere-351/homeworks/7/problem_4.py
+++ aere-351/homeworks/7/problem_4.py
@@ -5,11 +5,13 @@ ur = pint.UnitRegistry()
 km = ur.kilometer
 s = ur.second
 deg = ur.degree
+rad = ur.radian
 
 mu = 3.98600e5 * (km**3) / (s**2)
 
 a = 12000 * km
 e = 0.2
+p = a * (1 - e**2)
 
 omega_1 = 15 * deg
 i_1 = 120 * deg
@@ -23,12 +25,14 @@ delta_i = i_2 - i_1
 
 if (delta_Omega > 0 and delta_i > 0) or (delta_Omega < 0 and delta_i < 0):
     # Concordant
+
     raise NotImplementedError("I don't have the willpower")
 else:
     # Discordant
+
     alpha = acos(cos(i_1) * cos(i_2) + sin(i_1) * sin(i_2) * cos(delta_Omega))
 
-    u_1_c = acos((cos(alpha) * cos(i_1) - cos(i_2)) / (sin(alpha) * sin(i_1)))
+    u_1_c = acos((cos(i_2) - cos(alpha) * cos(i_1)) / (sin(alpha) * sin(i_1)))
     u_2_c = acos((cos(alpha) * cos(i_2) - cos(i_1)) / (sin(alpha) * sin(i_2)))
     u_1_s = asin((sin(delta_Omega) * sin(i_2)) / sin(alpha))
     u_2_s = asin((sin(delta_Omega) * sin(i_1)) / sin(alpha))
@@ -40,11 +44,10 @@ else:
     theta_2 = theta_1
     omega_2 = 2 * pi - u_2 - theta_2
 
-    p = a * (1 - e**2)
     v_theta = ((mu / p) ** (1 / 2)) * (1 + e * cos(theta_1))
     delta_v = 2 * v_theta * sin(alpha / 2)
 
-    print(f"theta_1 = {theta_1.to(deg)}")
-    print(f"theta_2 = {theta_2.to(deg)}")
+    print(f"theta_1 = {theta_1.to(rad)}")
+    print(f"theta_2 = {theta_2.to(rad)}")
     print(f"delta_v = {delta_v}")
-    print(f"omega_2 = {omega_2.to(deg)}")
+    print(f"omega_2 = {omega_2.to(rad)}")
