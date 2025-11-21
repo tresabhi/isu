f4dc7934334a944419c03462e9c26b421e2df2f4
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-11-21 10:26:20 -0600

    p1

---
+++ aere-362/labs/11/problem_1/runScript.m
@@ -0,0 +1,15 @@
+theta = [0.6; 0.3];
+t = [1; 0.5];
+gamma = [1; 1];
+d = [1; 1];
+
+for i = 1:10
+    gamma = aeroComp(theta, d);
+    d = structComp(theta, t, gamma);
+end
+
+disp("gamma =")
+disp(gamma);
+
+disp("d =")
+disp(d);
