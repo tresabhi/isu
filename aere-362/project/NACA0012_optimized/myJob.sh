7ca87d0aad7a1932cb7b647a83774a13133cfe2b
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-12-04 17:16:51 -0600

    add temp

---
+++ aere-362/project/NACA0012_optimized/myJob.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=0:15:00           # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1                # number of nodes
+#SBATCH --ntasks-per-node=16     # CPU cores per node 
+#SBATCH --output="log-%j.txt"    # simulation log file name
+#SBATCH --partition=instruction  # node type
+#SBATCH --account=f2025.aere.3620.01
+#SBATCH --constraint=intel
+
+. /work/class-faculty/phe/dafoam/loadDAFoam.sh
+chmod -R 750 *
+./preProcessing.sh
+mpirun -np 16 python runScript.py
+reconstructPar
+rm -rf processor*
