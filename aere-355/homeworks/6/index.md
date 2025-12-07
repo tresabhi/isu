ec5e7fb0af13a7f632fb067ba9005ed9f4b589b7
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-12-06 21:18:34 -0600

    plot

--- aere-355/homeworks/6/index.md
+++ aere-355/homeworks/6/index.md
@@ -1,3 +1,48 @@
 # AERE 355 Homework 6
 
 ## 1.
+
+Following the matrix equation listed on page 9 in the chapter 5 notes, along with `ode45`, I came up with the following MATLAB code (with an anonymous function wrapping the `dynamics` function to pass the variables declared in `plot_dynamics`):
+
+```m
+plot_dynamics(deg2rad(2));
+plot_dynamics(deg2rad(4));
+legend('\delta_r = 2 deg', '\delta_r = 4 deg');
+
+function plot_dynamics(delta_r)
+    zeta = 0.2;
+    omega_n = 3.142;
+    g_s = -0.9;
+
+    T = 4 * 2 * pi / omega_n;
+
+    x_0 = [0; 0];
+
+    [t, x] = ode45(@(~, x) dynamics(x, zeta, omega_n, g_s, delta_r), [0, T], x_0);
+
+    plot(t, x(:, 2));
+    hold on;
+    grid on;
+
+    xlabel('Time (s)');
+    ylabel('\psi (deg)');
+
+    function x_dot = dynamics(x, zeta, omega_n, g_s, delta_r)
+        A = [
+             -2 * zeta * omega_n, -omega_n ^ 2;
+             1, 0
+             ];
+        B = [
+             g_s * omega_n ^ 2;
+             0
+             ];
+
+        x_dot = A * x + B * delta_r;
+    end
+
+end
+```
+
+This creates a pretty plot:
+
+![](https://i.imgur.com/cb7mblw.png)
--- aere-355/homeworks/6/problem_1_a.m
+++ aere-355/homeworks/6/problem_1_a.m
@@ -1,27 +1,36 @@
-zeta = 0.2;
-omega_n = 3.142;
-g_s = -0.9;
+plot_dynamics(deg2rad(2));
+plot_dynamics(deg2rad(4));
+legend('\delta_r = 2 deg', '\delta_r = 4 deg');
 
-T = 4 * 2 * pi / omega_n;
+function plot_dynamics(delta_r)
+    zeta = 0.2;
+    omega_n = 3.142;
+    g_s = -0.9;
 
-x_0 = [0; 0];
-delta_r = deg2rad(2);
+    T = 4 * 2 * pi / omega_n;
 
-[t, x] = ode45(@(~, x) dynamics(x, zeta, omega_n, g_s, delta_r), [0, T], x_0);
+    x_0 = [0; 0];
 
-plot(t, x(:, 2)); hold on;
-xlabel('Time (s)');
-ylabel('\psi (deg)');
+    [t, x] = ode45(@(~, x) dynamics(x, zeta, omega_n, g_s, delta_r), [0, T], x_0);
 
-function x_dot = dynamics(x, zeta, omega_n, g_s, delta_r)
-    A = [
-         -2 * zeta * omega_n, -omega_n ^ 2;
-         1, 0
-         ];
-    B = [
-         g_s * omega_n ^ 2;
-         0
-         ];
+    plot(t, x(:, 2));
+    hold on;
+    grid on;
 
-    x_dot = A * x + B * delta_r;
+    xlabel('Time (s)');
+    ylabel('\psi (deg)');
+
+    function x_dot = dynamics(x, zeta, omega_n, g_s, delta_r)
+        A = [
+             -2 * zeta * omega_n, -omega_n ^ 2;
+             1, 0
+             ];
+        B = [
+             g_s * omega_n ^ 2;
+             0
+             ];
+
+        x_dot = A * x + B * delta_r;
+    end
+
 end
