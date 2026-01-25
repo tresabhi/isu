e35a2085c174d93ec2b88f892e63f4973cae863f
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-01-24 21:22:31 -0600

    2

---
+++ aere-311/homeworks/1/problem_2.py
@@ -0,0 +1,26 @@
+import pint
+from tabulate import tabulate
+
+ur = pint.UnitRegistry()
+
+R = 1716.46 * ur.ft * ur.lb / (ur.slug * ur.rankine)
+T = 936 * ur.rankine
+p = 7.8 * ur.atm
+
+gamma = 1.4
+
+c_v = R / (gamma - 1)
+c_p = R + c_v
+e = c_v * T
+h = c_p * T
+
+print(
+    tabulate(
+        [
+            ["c_p", c_p.to(ur.ft * ur.lb / (ur.slug * ur.rankine))],
+            ["c_v", c_v.to(ur.ft * ur.lb / (ur.slug * ur.rankine))],
+            ["e", e.to(ur.ft * ur.lb / ur.slug)],
+            ["h", h.to(ur.ft * ur.lb / ur.slug)],
+        ]
+    )
+)
