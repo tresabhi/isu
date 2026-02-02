14dbf85c32545f2f716042ba5005a9300e1cef01
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-02-02 10:18:47 -0600

    some more equatiosn

--- aere-311/solver.py
+++ aere-311/solver.py
@@ -1,22 +1,32 @@
 from sympy import symbols, Eq, solve
+from tabulate import tabulate
 import pint
 
-ur = pint.UnitRegistry()
+ur = pint.UnitRegistry(system="mks")
 
 T_0, T = symbols("T_0 T")
+rho_0, rho = symbols("rho_0 rho")
+P_0, P = symbols("rho_0 rho")
+
 u, M = symbols("u M")
 gamma = symbols("gamma")
+c_p, c_v, R = symbols("c_p c_v R")
 
 equations = [
     Eq(T_0 / T, (1 + ((gamma - 1) / 2) * M**2)),
+    Eq(rho_0 / rho, (T_0 / T) ** (c_v / R)),
+    Eq(P_0 / P, (T_0 / T) ** (c_p / R)),
 ]
+
 knowns = {
     gamma: 7 / 5,
     M: 2.6,
-    T_0: (269.15 * ur.K).magnitude,
+    T_0: 269.15 * ur.K,
 }
 
-equations = [eq.subs(knowns) for eq in equations]
-sol = solve(equations, dict=True)
+knowns = {key: value.to_base_units().magnitude for key, value in knowns.items()}
 
-print(sol)
+equations = [equation.subs(knowns) for equation in equations]
+solutions = solve(equations)
+
+print(tabulate(solutions.items()))
