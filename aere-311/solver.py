b62286c2374a1730de6988a4d361a81a694abd7c
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-02-02 10:41:28 -0600

    units

--- aere-311/solver.py
+++ aere-311/solver.py
@@ -1,22 +1,43 @@
 from sympy import symbols, Eq, solve
 from tabulate import tabulate
-import pint
+from pint import UnitRegistry, Quantity
 
-ur = pint.UnitRegistry(system="mks")
+ur = UnitRegistry(system="mks")
+
+u, M = symbols("u M")
+
+gamma = symbols("gamma")
+c_p, c_v, R = symbols("c_p c_v R")
 
 T_0, T = symbols("T_0 T")
 rho_0, rho = symbols("rho_0 rho")
 P_0, P = symbols("rho_0 rho")
-
-u, M = symbols("u M")
-gamma = symbols("gamma")
-c_p, c_v, R = symbols("c_p c_v R")
 
 equations = [
     Eq(T_0 / T, (1 + ((gamma - 1) / 2) * M**2)),
     Eq(rho_0 / rho, (T_0 / T) ** (c_v / R)),
     Eq(P_0 / P, (T_0 / T) ** (c_p / R)),
 ]
+units = {
+    u: ur.m / ur.s,
+    M: ur.dimensionless,
+}
+units = {
+    **units,
+    gamma: ur.dimensionless,
+    c_p: ur.J / (ur.kg * ur.K),
+    c_v: ur.J / (ur.kg * ur.K),
+    R: ur.J / (ur.kg * ur.K),
+}
+units = {
+    **units,
+    T_0: ur.K,
+    T: ur.K,
+    rho_0: ur.kg / ur.m**3,
+    rho: ur.kg / ur.m**3,
+    P_0: ur.Pa,
+    P: ur.Pa,
+}
 
 knowns = {
     gamma: 7 / 5,
@@ -24,9 +45,14 @@ knowns = {
     T_0: 269.15 * ur.K,
 }
 
-knowns = {key: value.to_base_units().magnitude for key, value in knowns.items()}
+knowns = {
+    key: (value.to(units[key]).magnitude if isinstance(value, Quantity) else value)
+    for key, value in knowns.items()
+}
 
 equations = [equation.subs(knowns) for equation in equations]
 solutions = solve(equations)
 
-print(tabulate(solutions.items()))
+for index, solution in enumerate(solutions):
+    print(f"Solution set {index + 1}")
+    print(tabulate(solution.items()), end="\n\n")
