91fa7afee0e025308328d9f387f80ba5b90682eb
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-02-06 10:30:28 -0600

    moar!!

--- aere-311/solver/equations.py
+++ aere-311/solver/equations.py
@@ -4,33 +4,51 @@ from symbols import *
 isentropic_equations = [
     sp.Eq(p, rho * R * T),
     sp.Eq(p_0, rho_0 * R * T_0),
+    sp.Eq(p_inf, rho_inf * R * T_inf),
     #
     sp.Eq(a, (gamma * R * T) ** (1 / 2)),
     sp.Eq(a_0, (gamma * R * T_0) ** (1 / 2)),
+    sp.Eq(a_inf, (gamma * R * T_inf) ** (1 / 2)),
     #
     sp.Eq(a, (gamma * p / rho) ** (1 / 2)),
     sp.Eq(a_0, (gamma * p_0 / rho_0) ** (1 / 2)),
+    sp.Eq(a_inf, (gamma * p_inf / rho_inf) ** (1 / 2)),
     #
     sp.Eq(M, u / a),
     sp.Eq(M_0, u_0 / a_0),
+    sp.Eq(M_inf, u_inf / a_inf),
     #
     sp.Eq(M, u / ((gamma * R * T) ** (1 / 2))),
     sp.Eq(M_0, u_0 / ((gamma * R * T_0) ** (1 / 2))),
+    sp.Eq(M_inf, u_inf / ((gamma * R * T_inf) ** (1 / 2))),
     #
     sp.Eq(M, u * sp.sqrt((1 + ((gamma - 1) / 2) * M**2) / (gamma * R * T_0))),
+    sp.Eq(
+        M_inf, u_inf * sp.sqrt((1 + ((gamma - 1) / 2) * M_inf**2) / (gamma * R * T_inf))
+    ),
     #
     sp.Eq(T_0_T, 1 + ((gamma - 1) / 2) * M**2),
+    sp.Eq(T_0_T_inf, 1 + ((gamma - 1) / 2) * M_inf**2),
+    #
     sp.Eq(p_0_p, (1 + (gamma - 1) / 2 * M**2) ** (gamma / (gamma - 1))),
-    sp.Eq(rho_0_rho / rho, (1 + (gamma - 1) / 2 * M**2) ** (1 / (gamma - 1))),
+    sp.Eq(p_0_p_inf, (1 + (gamma - 1) / 2 * M_inf**2) ** (gamma / (gamma - 1))),
+    #
+    sp.Eq(rho_0_rho, (1 + (gamma - 1) / 2 * M**2) ** (1 / (gamma - 1))),
+    sp.Eq(rho_0_rho_inf, (1 + (gamma - 1) / 2 * M_inf**2) ** (1 / (gamma - 1))),
     #
     sp.Eq(T_0_T, T_0 / T),
+    sp.Eq(T_0_T_inf, T_0 / T_inf),
+    #
     sp.Eq(p_0_p, p_0 / p),
+    sp.Eq(p_0_p_inf, p_0 / p_inf),
+    #
     sp.Eq(rho_0_rho, rho_0 / rho),
+    sp.Eq(rho_0_rho_inf, rho_0 / rho_inf),
 ]
 
-with open("test.md", "w") as file:
-    for equation in isentropic_equations:
-        file.write(f"$$\n{sp.latex(equation)}\n$$\n\n")
+# with open("test.md", "w") as file:
+#     for equation in isentropic_equations:
+#         file.write(f"$$\n{sp.latex(equation)}\n$$\n\n")
 
 bernoulli_equations = [
     sp.Eq(q, (1 / 2) * rho * u**2),
--- aere-311/solver/symbols.py
+++ aere-311/solver/symbols.py
@@ -1,13 +1,18 @@
 from sympy import symbols
 
-u, u_0 = symbols("u u_0")
-a, a_0 = symbols("a a_0")
-M, M_0 = symbols("M M_0")
+u, u_0, u_inf = symbols("u u_0 u_inf")
+a, a_0, a_inf = symbols("a a_0 a_inf")
+M, M_0, M_inf = symbols("M M_0 M_inf")
 
-p, p_0, p_0_p = symbols("p p_0 p_0_p")
-rho, rho_0, rho_0_rho = symbols("rho rho_0 rho_0_rho")
-T, T_0, T_0_T = symbols("T T_0 T_0_T")
-q, q_0 = symbols("q q_0")
+p, p_0, p_inf = symbols("p p_0 p_inf")
+rho, rho_0, rho_inf = symbols("rho rho_0 rho_inf")
+T, T_0, T_inf = symbols("T T_0 T_inf")
+
+p_0_p, p_0_p_inf = symbols("p_0_p p_0_p_inf")
+rho_0_rho, rho_0_rho_inf = symbols("rho_0_rho rho_0_rho_inf")
+T_0_T, T_0_T_inf = symbols("T_0_T T_0_T_inf")
+
+q, q_0, q_inf = symbols("q q_0 q_inf")
 
 gamma = symbols("gamma")
 c_p, c_v, R = symbols("c_p c_v R")
