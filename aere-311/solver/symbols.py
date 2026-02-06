2c89784eeb4cef555273ccab8e4260d068db8c25
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-02-06 10:50:39 -0600

    star equations

--- aere-311/solver/symbols.py
+++ aere-311/solver/symbols.py
@@ -1,18 +1,20 @@
 from sympy import symbols
 
-u, u_0, u_inf = symbols("u u_0 u_inf")
-a, a_0, a_inf = symbols("a a_0 a_inf")
-M, M_0, M_inf = symbols("M M_0 M_inf")
+u, u_0, u_inf, u_star = symbols("u u_0 u_inf u^*")
+a, a_0, a_inf, a_star = symbols("a a_0 a_inf a^*")
+M, M_0, M_inf, M_star = symbols("M M_0 M_inf M^*")
 
-p, p_0, p_inf = symbols("p p_0 p_inf")
-rho, rho_0, rho_inf = symbols("rho rho_0 rho_inf")
-T, T_0, T_inf = symbols("T T_0 T_inf")
+p, p_0, p_inf, p_star = symbols("p p_0 p_inf p^*")
+rho, rho_0, rho_inf, rho_star = symbols("rho rho_0 rho_inf rho^*")
+T, T_0, T_inf, T_star = symbols("T T_0 T_inf T^*")
 
-p_0_p, p_0_p_inf = symbols("p_0/p p_0/p_inf")
-rho_0_rho, rho_0_rho_inf = symbols("rho_0/rho rho_0/rho_inf")
-T_0_T, T_0_T_inf = symbols("T_0/T T_0/T_inf")
+p_0_p, p_0_p_inf, p_0_p_star = symbols("p_0/p p_0/p_inf p_0/p^*")
+rho_0_rho, rho_0_rho_inf, rho_0_rho_star = symbols(
+    "rho_0/rho rho_0/rho_inf rho_0/rho^*"
+)
+T_0_T, T_0_T_inf, T_0_T_star = symbols("T_0/T T_0/T_inf T_0/T^*")
 
-q, q_0, q_inf = symbols("q q_0 q_inf")
+q, q_0, q_inf, q_star = symbols("q q_0 q_inf q*")
 
 gamma = symbols("gamma")
 c_p, c_v, R = symbols("c_p c_v R")
--- aere-311/solver/units.py
+++ aere-311/solver/units.py
@@ -5,30 +5,37 @@ base_units = {
     u: ur.m / ur.s,
     u_0: ur.m / ur.s,
     u_inf: ur.m / ur.s,
+    u_star: ur.m / ur.s,
     #
     a: ur.m / ur.s,
     a_0: ur.m / ur.s,
     a_inf: ur.m / ur.s,
+    a_star: ur.m / ur.s,
     #
     M: ur.dimensionless,
     M_0: ur.dimensionless,
     M_inf: ur.dimensionless,
+    M_star: ur.dimensionless,
     #
     p: ur.Pa,
     p_0: ur.Pa,
     p_inf: ur.Pa,
+    p_star: ur.Pa,
     #
     q: ur.Pa,
     q_0: ur.Pa,
     q_inf: ur.Pa,
+    q_star: ur.Pa,
     #
     rho: ur.kg / ur.m**3,
     rho_0: ur.kg / ur.m**3,
     rho_inf: ur.kg / ur.m**3,
+    rho_star: ur.kg / ur.m**3,
     #
     T: ur.K,
     T_0: ur.K,
     T_inf: ur.K,
+    T_star: ur.K,
     #
     gamma: ur.dimensionless,
     c_p: ur.J / (ur.kg * ur.K),
@@ -37,10 +44,15 @@ base_units = {
     #
     T_0_T: ur.dimensionless,
     T_0_T_inf: ur.dimensionless,
+    T_0_T_star: ur.dimensionless,
+    #
     p_0_p: ur.dimensionless,
     p_0_p_inf: ur.dimensionless,
+    p_0_p_star: ur.dimensionless,
+    #
     rho_0_rho: ur.dimensionless,
     rho_0_rho_inf: ur.dimensionless,
+    rho_0_rho_star: ur.dimensionless,
 }
 
 output_units = {
