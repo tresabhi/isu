a45e88280d7c4179db0f6341886d5cd21626c584
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-02-11 15:57:33 -0600

    voltage reading

--- aere-344/labs/3/process.py
+++ aere-344/labs/3/process.py
@@ -4,13 +4,28 @@ CALIBRATION_POINTS = 5
 
 for index in range(CALIBRATION_POINTS):
     name = index + 1
+
+    voltages = []
     pressures = []
 
-    with open(f"data/calibration/{name}.csv", newline="") as file:
+    with open(f"data/calibration/{name}.txt") as file:
+        lines = file.readlines()[5:]
+
+        for line in lines:
+            magnitude = float(line[-12:-4])
+            power = float(line[-2])
+            voltage = magnitude * 10**power
+
+            voltages.append(voltage)
+
+    with open(f"data/calibration/{name}.csv") as file:
         reader = csv.reader(file)
 
         for row in reader:
-            pressures.append(float(row[2]))
+            pressure = float(row[2])
+            pressures.append(pressure)
 
     pressure = sum(pressures) / len(pressures)
-    print(pressure)
+    voltage = sum(voltages) / len(voltages)
+
+    print(voltage, pressure)
