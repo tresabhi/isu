e394de79828603f9c41a4be263f2382f2fd13958
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-02-12 11:01:01 -0600

    plot

--- aere-344/quizzes/4/main.m
+++ aere-344/quizzes/4/main.m
@@ -8,7 +8,8 @@ avgtbl = mean(tbl(:, 2));
 time = table2array(tbl(:, 1));
 value = table2array(tbl(:, 2));
 
-plot(time, value)
+plot(time, value, ".", "Color", "#a0a0FF");
+hold on;
 xlabel('Time (s)');
 ylabel('Value');
 title('Evolution of Data Points in Time');
@@ -18,6 +19,10 @@ N = length(time);
 delta = N * sum(time .^ 2) - ((sum(time)) .^ 2);
 m = ((N .* sum(time .* value)) - (sum(time) .* sum(value))) ./ delta;
 b = ((sum(value) .* sum(time .^ 2)) - (sum(time .* value) .* sum(time))) / delta;
+
+x = min(time):max(time);
+y = m * x + b;
+plot(x, y, 'Color', '#0000FF', 'LineStyle', '--')
 
 sigma = std(value - (m .* time) - b);
 sigmam = sqrt(N * (sigma ^ 2) / delta);
