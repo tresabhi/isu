9719263a6811626f9a5125518ace3cc269358485
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-03-05 10:51:17 -0600

    plots

--- aere-344/quizzes/7/main.py
+++ aere-344/quizzes/7/main.py
@@ -10,7 +10,7 @@ with open("data.dat") as file:
         lines[41:70],
         lines[71:99],
     ]
-    zone_index = 1
+    zone_index = 0
 
     for zone in zones:
         C_D = 0
@@ -19,30 +19,32 @@ with open("data.dat") as file:
             for line in zone
         ]
         index = 0
+
         xs = []
         ys = []
 
         for columns in zone:
             columns_prev = zone[0 if index == 0 else index - 1]
 
-            [_, y, u, _, _, _, _] = columns
+            [_, y, u, _, uu, _, _] = columns
             [_, y_prev, _, _, _, _, _] = columns_prev
 
             delta_y_i = y - y_prev
             C_D += u * (1 - u) * delta_y_i
+
+            xs.append(uu)
+            ys.append(y / c)
+
             index += 1
 
-            xs.append(u)
-            ys.append(y)
-
         C_D *= 2 / c
+        zone_index += 1
 
         print(f"C_D_{zone_index} = {C_D:.5g}")
 
         plt.figure()
         plt.title(f"Zone {zone_index}")
+
         plt.plot(xs, ys)
 
-        zone_index += 1
-
-plt.show()
+        plt.savefig(f"out/zone_{zone_index}.png")
