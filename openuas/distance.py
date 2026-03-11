189f8425e78d4f5855776a522f361b642739943b
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-03-11 15:52:39 -0500

    distances

---
+++ openuas/distance.py
@@ -0,0 +1,17 @@
+true_min = 1
+true_max = 4
+
+width = 0.4
+padding = width * 3 / 2
+gap = width / 4
+
+x1_min = true_min - padding + width / 2
+x1_max = true_max + padding + width / 2
+
+x2_min = true_min - padding - width / 2
+x2_max = true_max + padding - width / 2
+
+print(f"x1_min = {x1_min}")
+print(f"x1_max = {x1_max}")
+print(f"x2_min = {x2_min}")
+print(f"x2_max = {x2_max}")
