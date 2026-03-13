32a7d7c995878f5e346fd02bb39d038027bedf88
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-03-12 21:09:21 -0500

    hmmm

--- aere-311/solver/__main__.py
+++ aere-311/solver/__main__.py
@@ -15,19 +15,38 @@ solver = Solver(
         *shock_static_equations,
         *state_equations,
         *specific_heat_equations,
-        *prandtl_meyer_equations,
+        *oblique_shocks,
     ],
-    output_units=imperial_output_units,
+    output_units=durbin_output_units,
 )
 
 if __name__ == "__main__":
-    solver.solve(
+    deflection1 = solver.solve(
         {
             **air,
             #
-            M1: 2,
-            p1: 0.7 * ur.atm,
-            theta: 23.38 * ur.degree,
-            T1: 550 * ur.rankine,
+            M1: 3,
+            T1: 280 * ur.K,
+            p1: 3 * ur.atm,
+            theta: 30.6 * ur.deg,
+        }
+    )
+
+    solver.equations = [
+        *composite_equations,
+        *shock_static_equations,
+        *state_equations,
+        *specific_heat_equations,
+        *prandtl_meyer_equations,
+    ]
+
+    deflection2 = solver.solve(
+        {
+            **air,
+            #
+            M1: deflection1[M2],
+            T1: deflection1[T2],
+            p1: deflection1[p2],
+            theta: 30.6 * ur.deg,
         }
     )
