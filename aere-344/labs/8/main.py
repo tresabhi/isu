f21973faef76009a771846d629fa48ee3198f3d9
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-03-28 18:11:52 -0500

    some plotting

--- aere-344/labs/8/main.py
+++ aere-344/labs/8/main.py
@@ -1,35 +1,63 @@
 import csv
+import math
 import numpy as np
+import matplotlib.pyplot as plt
+
+in_to_m = 1 / 39.37
+
+u_inf = 13
+rho = 1.225
+delta = 4 / 1000
 
 xs_in = list(range(10)) + list(range(10, 65 + 5, 5))
-u_inf = 13
+
+rakes = []
 
 for x_in in xs_in:
     path = f"data/{x_in}in.csv"
+    x = x_in * in_to_m
 
     with open(path) as file:
         reader = csv.reader(file)
 
-        chunk1, chunk2, chunk3 = [np.array([0.0] * 16) for _ in range(3)]
+        rake_samples = []
+        total_samples = []
+        static_samples = []
         n = 0
 
         for row in reader:
             row = [float(cell) for cell in row]
 
-            chunk1_i = np.array(row[2:18])
-            chunk2_i = np.array(row[36:52])
-            chunk3_i = np.array(row[70:86])
+            chunk_1 = row[2:18]
+            chunk_2 = row[36:52]
+            chunk_3 = row[70:86]
 
-            chunk1 += chunk1_i
-            chunk2 += chunk2_i
-            chunk3 += chunk3_i
+            sample = chunk_1 + chunk_2 + chunk_3
+            rake_sample = np.array(sample[0:38])
+            total_sample = np.array(sample[38])
+            static_sample = np.array(sample[39])
+
+            rake_samples.append(rake_sample)
+            total_samples.append(total_sample)
+            static_samples.append(static_sample)
 
             n += 1
 
-        chunk1 /= n
-        chunk2 /= n
-        chunk3 /= n
+        rake = np.mean(rake_samples, axis=0)
 
-        # print(chunk1)
-        # print(chunk2)
-        # print(chunk3)
+        rakes.append(rake)
+
+        p0 = np.mean(total_samples)
+        p = np.mean(static_samples)
+
+        # u = math.sqrt((2 * (p0 - p)) / rho)
+
+xs = [x_in * in_to_m for x_in in xs_in]
+ys = [delta * i for i in range(1, 39)]
+
+x, y = np.meshgrid(xs, ys)
+z = np.array(rakes).T
+
+plt.contourf(x, y, z)
+plt.colorbar(label="Velocity")
+plt.savefig("out/1.png")
