f05c3f2c39adaedab1f81147097fca4d74478298
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-03-28 19:02:21 -0500

    ugh

--- aere-344/labs/8/main.py
+++ aere-344/labs/8/main.py
@@ -2,16 +2,27 @@ import csv
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 
 in_to_m = 1 / 39.37
 
 u_inf = 13
 rho = 1.225
-delta = 4 / 1000
+nu = 1.57e-5
+Delta_y = 4 / 1000
+
+is_ = list(range(1, 39))
+
+broken_indices = [56]
 
 xs_in = list(range(10)) + list(range(10, 65 + 5, 5))
+xs = [x_in * in_to_m for x_in in xs_in]
+ys = [Delta_y * i for i in is_]
 
-rakes = []
+xs = np.array(xs)
+ys = np.array(ys)
+
+profiles = []
 
 for x_in in xs_in:
     path = f"data/{x_in}in.csv"
@@ -23,7 +34,6 @@ for x_in in xs_in:
         rake_samples = []
         total_samples = []
         static_samples = []
-        n = 0
 
         for row in reader:
             row = [float(cell) for cell in row]
@@ -41,23 +51,37 @@ for x_in in xs_in:
             total_samples.append(total_sample)
             static_samples.append(static_sample)
 
-            n += 1
+        p0 = np.mean(rake_samples, axis=0)
+        p0_inf = np.mean(total_samples)
+        p_inf = np.mean(static_samples)
 
-        rake = np.mean(rake_samples, axis=0)
+        u = np.sqrt((2 * np.abs(p0 - p_inf)) / rho)
+        u_inf = np.sqrt((2 * (p0_inf - p_inf)) / rho)
 
-        rakes.append(rake)
+        Re_x = u * x / nu
 
-        p0 = np.mean(total_samples)
-        p = np.mean(static_samples)
+        profiles.append(u)
 
         # u = math.sqrt((2 * (p0 - p)) / rho)
 
-xs = [x_in * in_to_m for x_in in xs_in]
-ys = [delta * i for i in range(1, 39)]
 
-x, y = np.meshgrid(xs, ys)
-z = np.array(rakes).T
+ys_mm = ys * 1000
 
-plt.contourf(x, y, z)
-plt.colorbar(label="Velocity")
-plt.savefig("out/1.png")
+x, y = np.meshgrid(xs_in, ys_mm)
+z = np.array(profiles).T
+
+plt.figure(1)
+
+plt.contourf(x, y, z, levels=2**8)
+plt.colorbar(label="Velocity [m/s]")
+
+plt.xlabel("x [in]")
+plt.ylabel("y [mm]")
+
+plt.title("Velocity profile vs x (linear)")
+plt.savefig("out/velocity_profile_vs_x_linear.png")
+
+plt.yscale("log")
+
+plt.title("Velocity profile vs x (logarithmic)")
+plt.savefig("out/velocity_profile_vs_x_logarithmic.png")
