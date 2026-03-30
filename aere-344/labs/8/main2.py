a26ab041771d9d7d325761b6e318e0e54b481ba7
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-03-30 17:55:35 -0500

    try again

---
+++ aere-344/labs/8/main2.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+rho = 1.225
+Delta_y_mm = 4
+Delta_y = Delta_y_mm / 1000
+
+xs_in = list(range(10)) + list(range(10, 65 + 5, 5))
+ys = np.array(range(39)) * Delta_y
+
+velocity_profiles = []
+y_profiles = []
+deltas = []
+
+for x_in in xs_in:
+    path = f"data/{x_in}in.csv"
+
+    with open(path) as file:
+        reader = csv.reader(file)
+        samples = []
+
+        for row in reader:
+            row = [float(cell) for cell in row]
+
+            chunk_1 = row[2:18]
+            chunk_2 = row[36:52]
+            chunk_3 = row[70:86]
+
+            sample = chunk_1 + chunk_2 + chunk_3
+            samples.append(sample)
+
+        mean_sample = np.mean(samples, axis=0)
+
+        p_pitot = mean_sample[39 - 1]
+        p_static = mean_sample[40 - 1]
+        p_rake = np.array(mean_sample[0:38])
+
+        q_rake = np.maximum(0, p_rake - p_static)
+        q_rake = np.array([0] + list(q_rake))
+        q_pitot = p_pitot - p_static
+
+        u_inf = np.sqrt((2 / rho) * q_pitot)
+        u_rake = np.sqrt((2 / rho) * q_rake)
+
+        idx = np.argmax(u_rake >= 0.99 * u_inf)
+
+        u1 = u_rake[idx - 1]
+        u2 = u_rake[idx]
+        y1 = ys[idx - 1]
+        y2 = ys[idx]
+
+        u99 = 0.99 * u_inf
+        delta = y1 + ((u99 - u1) / (u2 - u1)) * (y2 - y1)
+        print(delta)
+        y_profile = ys / delta
+
+        velocity_profiles.append(u_rake)
+        y_profiles.append(y_profile)
+        deltas.append(delta)
+
+deltas = np.array(deltas)
+deltas_mm = deltas * 1000
+
+plt.figure(1)
+plt.title("Delta vs Downstream Position")
+plt.plot(xs_in, deltas_mm)
+plt.xlabel("x [in]")
+plt.ylabel("delta [mm]")
+
+plt.show()
