74c9aae624e13af7f1e93947390dc5434e316f80
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-04-09 12:57:48 -0500

    not like i remember lol

--- aere-344/quizzes/11/main.py
+++ aere-344/quizzes/11/main.py
@@ -39,37 +39,38 @@ def solve(pt):
     pe_pt = pe / pt
     Ae_At = Ae / At
 
-    def f(M):
-        return Ae_At - area_mach(M)
+    # def f(M):
+    #     return Ae_At - area_mach(M)
 
-    # Me_1 = math.sqrt(
-    #     (-1 / (gamma - 1))
-    #     + (
-    #         1 / ((gamma - 1) ** 2)
-    #         + (2 / (gamma - 1))
-    #         * (
-    #             ((2 / (gamma + 1)) ** ((gamma + 1) / (gamma - 1)))
-    #             / ((Ae_At * pe_pt) ** 2)
-    #         )
-    #     )
-    #     ** (1 / 2)
-    # )
-    # pt_pe_1 = (1 + ((gamma - 1) / 2) * Me_1**2) ** (gamma / (gamma - 1))
-
-    M = sympy.Symbol("M")
-
-    eq = Ae_At - (1 / M) * ((2 / (gamma + 1)) * (1 + (gamma - 1) / 2 * M**2)) ** (
-        (gamma + 1) / (2 * (gamma - 1))
+    Me_1 = math.sqrt(
+        (-1 / (gamma - 1))
+        + (
+            1 / ((gamma - 1) ** 2)
+            + (2 / (gamma - 1))
+            * (
+                ((2 / (gamma + 1)) ** ((gamma + 1) / (gamma - 1)))
+                / ((Ae_At * pe_pt) ** 2)
+            )
+        )
+        ** (1 / 2)
     )
+    pt_pe_1 = (1 + ((gamma - 1) / 2) * Me_1**2) ** (gamma / (gamma - 1))
 
-    M_sub = brentq(f, 1e-6, 0.999)
-    M_sup = brentq(f, 1.001, 10)
+    # M = sympy.Symbol("M")
+
+    # eq = Ae_At - (1 / M) * ((2 / (gamma + 1)) * (1 + (gamma - 1) / 2 * M**2)) ** (
+    #     (gamma + 1) / (2 * (gamma - 1))
+    # )
+
+    # M_sub = brentq(f, 1e-6, 0.999)
+    # M_sup = brentq(f, 1.001, 10)
 
     print(f"pt = {pt}")
     print(f"Ae/At = {Ae_At}")
-    print(f"M_sub = {M_sub}")
-    print(f"M_sup = {M_sup}")
-    # print(f"pt/pe (1st critical) = {pt_pe_1}")
+    print(f"Me (1st critical) = {Me_1}")
+    # print(f"M_sub = {M_sub}")
+    # print(f"M_sup = {M_sup}")
+    print(f"pt/pe (1st critical) = {pt_pe_1}")
     print()
 
 
