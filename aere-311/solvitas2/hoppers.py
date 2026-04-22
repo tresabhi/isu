6db0e5c63807cc6274a2d3f73652c15abc4ef03c
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-04-22 16:56:18 -0500

    good ol' solver

--- aere-311/solvitas2/hoppers.py
+++ aere-311/solvitas2/hoppers.py
@@ -26,13 +26,57 @@ class Hopper:
 
             print(f"{symbol}: {convoluted_value} -> {value}")
 
-            self.values[symbol] = value
+            self.values[symbol] = value.magnitude
 
         print()
+
+    def hop(self):
+        solved = 0
+        iteration = 0
+
+        while True:
+            print(f"Iteration {iteration + 1}:")
+
+            iteration += 1
+            solved = 0
+            index = 0
+
+            for equation in self.equations:
+                lhs, rhs = equation
+
+                lhs_subbed = lhs.subs(self.values)
+                rhs_subbed = rhs.subs(self.values)
+
+                free = lhs_subbed.free_symbols | rhs_subbed.free_symbols
+
+                print(f"  ({index + 1}): ", end="")
+
+                if len(free) > 1:
+                    print(f"{free} free symbols; skipping...")
+                elif len(free) == 1:
+                    symbol = free.pop()
+                    initial = symbol.initial
+
+                    value = sympy.nsolve(rhs_subbed - lhs_subbed, symbol, initial)
+
+                    self.values[symbol] = value
+                    solved += 1
+
+                    print(f"{symbol} = {value}")
+                else:
+                    print(f"{lhs_subbed} == {rhs_subbed}")
+
+                index += 1
+
+            print()
+
+            if solved == 0:
+                break
 
     def __init__(self, knowns):
         self.greet()
         self.knowns(knowns)
+        self.hop()
 
 
 class PerfectlyExpandedSubsonicNozzleHopper(Hopper):
--- aere-311/solvitas2/symbol.py
+++ aere-311/solvitas2/symbol.py
@@ -3,10 +3,11 @@ from registry import *
 
 
 class Symbol(sympy.Symbol):
-    def __new__(self, name: str, unit: str | None = None):
+    def __new__(self, name: str, unit: str | None = None, initial: float = 0.1):
         symbol = sympy.Symbol.__new__(self, name, positive=True, imaginary=False)
 
         symbol.name = name
         symbol.unit = ur("dimensionless" if unit is None else unit)
+        symbol.initial = initial
 
         return symbol
