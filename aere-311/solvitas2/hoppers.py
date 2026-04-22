9722b82cf06b90b4873a4243294784daf95d0002
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-04-22 16:38:32 -0500

    units

--- aere-311/solvitas2/hoppers.py
+++ aere-311/solvitas2/hoppers.py
@@ -1,9 +1,38 @@
 from symbols import *
+import pint
 
 
 class Hopper:
+    has_greeted = False
+
+    values = {}
+    givens = []
+
+    def greet(self):
+        if not self.__class__.has_greeted:
+            self.__class__.has_greeted = True
+            print("This is Solvitas 2.0\n")
+
+    def knowns(self, knowns):
+        for symbol, convoluted_value in knowns.items():
+            self.givens.append(symbol)
+
+            if isinstance(convoluted_value, pint.Quantity):
+                value = convoluted_value
+            else:
+                value = convoluted_value * symbol.unit
+
+            value = value.to(symbol.unit)
+
+            print(f"{symbol}: {convoluted_value} -> {value}")
+
+            self.values[symbol] = value
+
+        print()
+
     def __init__(self, knowns):
-        print(self.equations)
+        self.greet()
+        self.knowns(knowns)
 
 
 class PerfectlyExpandedSubsonicNozzleHopper(Hopper):
