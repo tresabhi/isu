ae08994b7cca00ca030e58221d03101cae9493ee
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-05-04 16:34:24 -0500

    3

--- aere-311/solvitas2/__main__.py
+++ aere-311/solvitas2/__main__.py
@@ -15,13 +15,13 @@ air = {
 s1 = LinearizedFlatPlate(
     {
         **air,
-        alpha: 15 * ur("deg"),
+        alpha: 30 * ur("deg"),
         M_inf: 2.600,
     }
 ).solve()
 
-true_cl = 0.452
-true_cd = 0.121
+true_cl = 1.19
+true_cd = 0.687
 
 e_cl = abs(s1[cl] - true_cl) / true_cl
 e_cd = abs(s1[cd] - true_cd) / true_cd
