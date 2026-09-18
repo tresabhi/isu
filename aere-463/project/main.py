e3e8afabc0ed0971808637c20291abef32def9e8
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-09-18 17:48:37 -0500

    starter files

--- aere-463/project/main.py
+++ aere-463/project/main.py
@@ -1,8 +1,5 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import math
 import numpy as np
-import time
 
 w = 3  # m
 h = 3  # m
@@ -64,76 +61,3 @@ def f(X, T):
         m += m_i
 
     return m
-
-
-def visualize(V, T):
-    t0 = time.time()
-
-    x = np.linspace(0, w, N + 1)
-    y = np.linspace(0, h, M + 1)
-    grid_x, grid_y = np.meshgrid(x, y)
-
-    z = V.flatten()
-
-    vertices_3d = np.column_stack((grid_x.ravel(), z, grid_y.ravel()))
-
-    triangles_3d = []
-    for i_tri in range(n_t):
-        is_bottom_left = i_tri % 2 == 0
-        i_quad = i_tri // 2
-        i_bottom_left_vertex = i_quad % N + (i_quad // N) * (N + 1)
-
-        if is_bottom_left:
-            i0 = i_bottom_left_vertex
-            i1 = i0 + 1
-            i2 = i0 + N + 1
-        else:
-            i0 = i_bottom_left_vertex + N + 2
-            i1 = i0 - 1
-            i2 = i_bottom_left_vertex + 1
-
-        triangles_3d.append(vertices_3d[[i0, i1, i2]])
-
-    thicknesses = T.flatten()
-
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111, projection="3d")
-
-    collection = Poly3DCollection(
-        triangles_3d, cmap="RdYlGn_r", edgecolors="none", alpha=1.0, shade=False
-    )
-    collection.set_array(thicknesses)
-
-    ax.add_collection3d(collection)
-    ax.set_xlim(0, w)
-
-    z_min, z_max = np.min(z), np.max(z)
-    if z_min == z_max:
-        ax.set_ylim(z_min - 0.1, z_max + 0.1)
-    else:
-        ax.set_ylim(z_min, z_max)
-
-    ax.set_zlim(0, h)
-
-    ax.set_xlabel("x [m]")
-    ax.set_ylabel("v [mm]")
-    ax.set_zlabel("y [m]")
-    ax.set_title("3D Armor Surface & Thickness Distribution")
-
-    cbar = fig.colorbar(collection, ax=ax, pad=0.1)
-    cbar.set_label("Armor Thickness (mm)")
-
-    # Adjust view angle to bring Z-offset out toward viewer
-    ax.view_init(elev=20, azim=-60)
-
-    plt.tight_layout()
-
-    print(time.time() - t0)
-
-    plt.show()
-
-
-visualize(Vs[-1], Ts[-1])
-
-def add(a, b):
-    return a + b
